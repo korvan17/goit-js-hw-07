c6ffca221d9b0b2603c40e1c4c491ce7b1d33745
Ivan Kornienko <ivan@homeds.pro>
2023-03-06 00:46:14 +0200

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const arrGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li>
+        <img
+        width = "320"
+        src="${preview}"
+        alt="${description}"
+        />
+    </li>`;
+  })
+  .join("");
+
+const gallery = document.querySelector(".gallery");
+const listGallerry = document.createElement("ul");
+listGallerry.insertAdjacentHTML("beforeend", arrGallery);
+gallery.appendChild(listGallerry);
