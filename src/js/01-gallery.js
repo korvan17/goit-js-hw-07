e139af991219faa83fa36bae2a5eef6750974068
Ivan Kornienko <ivan@homeds.pro>
2023-03-07 01:18:39 +0200

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,4 @@
+// import * as basicLightbox from "basiclightbox";
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
@@ -7,11 +8,11 @@ const arrGallery = galleryItems
   .map(({ preview, original, description }) => {
     return `
     <div class="gallery__item">
-        <a class="gallery__link" href="javascript:void(0)">
+        <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
             src="${preview}"
-            data-source="${original}.jpg"
+            data-source="${original}"
             alt="${description}"
             />
         </a>
@@ -21,3 +22,20 @@ const arrGallery = galleryItems
 
 const gallery = document.querySelector(".gallery");
 gallery.insertAdjacentHTML("beforeend", arrGallery);
+
+gallery.addEventListener("click", clickGallerayItem);
+
+function clickGallerayItem(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const largeImg = e.target.dataset.source;
+  console.log(largeImg);
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImg}" width="800" height="600">
+`);
+
+  instance.show();
+}
